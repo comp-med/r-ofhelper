9043b2f7f55ee70a04680271c9f552e525cfdf1e
cfbeuchel <carl.beuchel@charite.de>
2026-02-09 11:11:03 +0100

    tweak: allow separator selection in exlode function

--- R/explode_multi_select.R
+++ R/explode_multi_select.R
@@ -2,12 +2,18 @@
 
 explode_multi_select <- function(
   x,
+  answer_separators = c(",", "|"),
   long_format = FALSE,
   na_is_none_of_the_above = TRUE
 ) {
+  # TODO this should be a separate function
+  specials <- "([.|()\\^{}+$*?]|\\[|\\]|\\\\)"
+  escaped_seps <- gsub(specials, "\\\\\\1", answer_separators)
+  joint_seps <- paste(escaped_seps, collapse = "|")
+
   unique_entries <- unique(x)
   unique_entries <- gsub('\\[|\\]|\\"', "", unique_entries)
-  unique_entries <- strsplit(unique_entries, ",|\\|")
+  unique_entries <- strsplit(unique_entries, joint_seps)
   unique_entries <- unique(unlist(unique_entries))
 
   # This should be excluded
