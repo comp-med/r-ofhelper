5224e80e71cbf02846429eeb924ff84fd557e722
cfbeuchel <carl.beuchel@charite.de>
2026-02-19 09:05:16 +0100

    doc: add minimal documentation to dx_cache functions

--- R/dx_cache.R
+++ R/dx_cache.R
@@ -1,3 +1,8 @@
+#' Initialize the DNAnexus cache environment
+#'
+#' Creates a new environment to store DNAnexus configuration values.
+#'
+#' @return An environment with slots for storing DNAnexus configuration
 init_dx_cache <- function() {
   rlang::env(
     dx_binary = NULL,
@@ -12,21 +17,39 @@ init_dx_cache <- function() {
 .dx_cache <- init_dx_cache()
 
 
+#' Reset the DNAnexus cache
+#'
+#' Clears all stored DNAnexus configuration values, setting them to NULL
+#' except for the initialized flag which is set to FALSE.
 reset_dx_cache <- function() {
-  .dx_cache$dx_binary = NULL
-  .dx_cache$dx_project_id = NULL
-  .dx_cache$dx_project_name = NULL
-  .dx_cache$dx_path = NULL
-  .dx_cache$dx_user = NULL
-  .dx_cache$dx_server_host = NULL
-  .dx_cache$dx_initialized = FALSE
+  set_dx_cache(
+    dx_binary = NULL,
+    dx_project_id = NULL,
+    dx_project_name = NULL,
+    dx_path = NULL,
+    dx_user = NULL,
+    dx_server_host = NULL,
+    dx_initialized = FALSE
+  )
 }
 
-# named list, simply `"option" = value`
+#' Set values in the DNAnexus cache
+#'
+#' Assigns one or more values to the DNAnexus cache environment.
+#'
+#' @param ... Named arguments to set in the cache
+#' @return Invisible NULL
 set_dx_cache <- function(...) {
   rlang::env_bind(.dx_cache, ...)
 }
 
+#' Get values from the DNAnexus cache
+#'
+#' Retrieves one or more values from the DNAnexus cache environment.
+#'
+#' @param dx_property Character string specifying which property to retrieve.
+#'   Use "all" to retrieve all properties. Default is "all".
+#' @return Value(s) from the cache environment
 get_dx_cache <- function(dx_property = "all") {
   dx_properties <- c(
     "dx_binary",
@@ -51,19 +74,35 @@ get_dx_cache <- function(dx_property = "all") {
   if (length(cache_result) == 1) {
     unlist(cache_result, use.names = FALSE)
   } else {
-    (cache_result)
+    cache_result
   }
 }
 
+#' Remove all entries from the DNAnexus cache
+#'
+#' Removes all named entries from the DNAnexus cache environment.
+#' @return Invisible NULL
 remove_dx_cache <- function() {
   rlang::env_unbind(.dx_cache, rlang::env_names(.dx_cache))
 }
 
+#' Get the DNAnexus path
+#'
+#' Retrieves the currently set DNAnexus path from the cache after validating
+#' that a path is set.
+#'
+#' @return Character string containing the DNAnexus path
 dx_get_path <- function() {
   dx_check_path()
   get_dx_cache("dx_path")
 }
 
+#' Get the DNAnexus project ID
+#'
+#' Retrieves the currently set DNAnexus project ID from the cache after validating
+#' that a project is set.
+#'
+#' @return Character string containing the DNAnexus project ID
 dx_get_project <- function() {
   dx_check_project()
   get_dx_cache("dx_project_id")
