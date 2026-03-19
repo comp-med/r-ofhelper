ad8a3022601bbe783a67fad43fd34ff42e35ccb3
cfbeuchel <carl.beuchel@charite.de>
2026-03-19 13:34:36 +0100

    fix: broken dx_download test

--- tests/testthat/test-dx_download.R
+++ tests/testthat/test-dx_download.R
@@ -5,6 +5,7 @@ test_that("dx_download function exists and is properly exported", {
   expect_true(exists("dx_download"))
   expect_type(dx_download, "closure")
 })
+
 test_that("dx_download tries to extract gzipped files", {
   local_mocked_bindings(
     dx_is_initialized = function() invisible(TRUE),
@@ -18,6 +19,10 @@ test_that("dx_download tries to extract gzipped files", {
   tmpfile <- withr::local_tempfile(pattern = "tmpfile_", fileext = ".csv.gz")
   decompressed_tmpfile <- fs::path_ext_remove(tmpfile)
   fs::file_create(tmpfile)
-  dx_download(files = tmpfile, unzip_files = TRUE)
+  dx_download(
+    files = tmpfile,
+    unzip_files = TRUE,
+    local_dir = fs::path_dir(tmpfile)
+  )
   expect_true(fs::file_exists(decompressed_tmpfile))
 })
